68c7f825720674d4353c961eb035cf1d0a541cbb
Luca Marchesi <argentlamilritorno@hotmail.it>
2018-01-06 00:02:49 +0100

    Struttura non funzionante

--- rubama/main.c
+++ rubama/main.c
@@ -4,5 +4,12 @@
 int main()
 {
     printf("Hello world!\n");
+    struct carta
+    {
+        int valore;
+        char seme[15];
+    };
+    struct carta luca={2,"picche"};
+    printf("Hai pescato %i di %c", luca.valore, luca.seme);
     return 0;
 }
